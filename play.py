40a813308160442d135317b14de75bc1aeb6cc87
ubuntulim <52439023+ubuntulim@users.noreply.github.com>
2020-06-09 10:43:02 +0900

    Update play.py

--- play.py
+++ play.py
@@ -1,28 +1,37 @@
+import os
+import time
 import subprocess
 import glob
 from pynput import keyboard
 
-
-soundlist = []
+soundlist = {}
 
 for file in glob.glob('drum_sound/*.wav'):
-   soundlist.extend({file})
+    _, filename = file.split('/')
+    key, _ = filename.split('.')
+    soundlist.update({key: file})
+
+sound = list(soundlist.keys())
+play = 'play'
+
+FNULL = open(os.devnull, 'w')
 
 def on_press(key):
     try:
+        if key == keyboard.Key.esc:
+            return False
         i = int(key.char)
-        if 0 <= i and i <= len(soundlist)-1 :      
-            subprocess.Popen(['play', soundlist[i]], stdout=subprocess.PIPE, stderr = subprocess.PIPE)
+        if 0 <= i and i <= len(sound)-1:
+            print_name = sound[i]
+            subprocess.Popen([play, soundlist[sound[i]]], stdout=FNULL, stderr = subprocess.STDOUT)
+        else:
+            print_name = 'None'
+
+        print(f"\t--> sound[{i+1}]: {print_name:20}", end='\r', flush=True)
     except AttributeError:
-        print('unexpected key {0} pressed'.format(key))
+        print(f"\t--> special key: {key} pressed", end='\r', flush=True)
+    except:
+        print(f'\t--> {type(key.char)}  {"":10}', end='\r', flush=True)
 
-
-def on_release(key):
-    print('{0} released'.format(key))
-    if key == keyboard.Key.esc:
-        return False
-
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
